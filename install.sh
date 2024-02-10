24684d5eb0db84d54ae6be86910712035571057a
DaHofa02 <65934943+DaHofa02@users.noreply.github.com>
2024-02-10 02:35:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 apt install python3-full && python3-pip
 pip3 install --break-system-packages influxdb-client
 pip3 install --break-system-packages python-dotenv
+pip3 install --break-system-packages requests
